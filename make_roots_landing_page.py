aea2994ab02eac81b871af8a6da314d27402f902
ALowVerus <aidan.low98@gmail.com>
2018-02-21 16:01:51 -0500

    Working towards a Jekyll environment.

--- make_roots_landing_page.py
+++ make_roots_landing_page.py
@@ -22,7 +22,8 @@ with tag('html', lang="en", klass="gr__blackrockdigital_github_io"):
         with tag('script', src="JSFiles/divheights.js"): pass # Custom JS
         # Pagination JS
         with tag('script', src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"): pass # Base JQuery
-        with tag('script', type="text/javascript", src="fullPage.js-master/jquery.fullPage.js"): pass # Pagination JS
+        #with tag('script', type="text/javascript", src="fullPage.js-master/jquery.fullPage.js"): pass  # Pagination JS Local
+        with tag('script', type="text/javascript", src="https://cdnjs.com/libraries/fullPage.js"): pass  # Pagination JS Online
 
     # Body
     with tag('body', id='page-top', data_gr_c_s_loaded="true"):
