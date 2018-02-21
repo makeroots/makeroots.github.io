0297cf118bccb00315cf23775bba9cfdbb1fc31c
ALowVerus <aidan.low98@gmail.com>
2018-02-21 17:59:42 -0500

    Fixing and consolidating files for Jekyll.

--- JSFiles/fullPageInit.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-	    anchors:['page_1, page_2, page_3, page_4, page_5'],
-	    navigation: true,
-		navigationPosition: 'right',
-		showActiveTooltip: false,
-	    navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch', 'Bleh'],
-	});
-});
