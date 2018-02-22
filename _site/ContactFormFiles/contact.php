bd11df926c1bf1f2e5c544d82e4dcb4406abaa78
Bryton Moeller <rytonbay@users.noreply.github.com>
2018-02-22 14:21:08 -0500

    Update contact.php

--- _site/ContactFormFiles/contact.php
+++ _site/ContactFormFiles/contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <makerootswebsite@website.com>';
+$from = 'Demo contact form <makerootsofficial@gmail.com>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'makerootsofficial@gmail.com';
