c832cec64e749ee8ad93107558d32487fabb8dc6
rytonbay <bryton@themoellers.net>
2018-02-24 21:17:06 -0500

    skldjf

--- _site/js/overlay.js
+++
@@ -1,9 +0,0 @@
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
-$(window).on('load', function() {
-   $("#cover").hide();
-});
